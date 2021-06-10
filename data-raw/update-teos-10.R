83be6eb94adf38e959c36e8c924060f43e3dc600
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-10 16:24:00 -0300

    fix building of C lib

--- data-raw/update-teos-10.R
+++ data-raw/update-teos-10.R
@@ -11,5 +11,12 @@ unzip("data-raw/gsw.zip", exdir = "data-raw")
 gsw_dir <- list.files("data-raw", "^GSW-C", full.names = TRUE)
 stopifnot(length(gsw_dir) == 1)
 
+# clear previous gsw_ files from src/, copy files
+unlink(list.files("src", "^gsw", full.names = TRUE))
 
-
+stopifnot(
+  file.copy(
+    file.path(gsw_dir, c("gswteos-10.h", "gsw_internal_const.h", "gsw_saar.c", "gsw_saar_data.c", "gsw_oceanographic_toolbox.c")),
+    "src"
+  )
+)
