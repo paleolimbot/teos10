ca41b27d3a194aedeac0ad114697c70cf0cab2e7
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-11 09:43:28 -0300

    wrap exactly one function

--- src/teos10-common.c
+++
@@ -1,7 +0,0 @@
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
-
-SEXP teos10_c_dummy() {
-    return R_NilValue;
-}
--- src/teos10-common.h
+++
@@ -1,5 +0,0 @@
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
-
-SEXP teos10_c_dummy();
